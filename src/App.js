299a448de077e1a7f8c79a7a2d4fc386b6eca105
Jean-yun <yesirchloe@gmail.com>
2024-08-07 19:43:09 +0900

    added dollar to coin amount function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   
+  //when change coin and type USD, return the amount of Coin
+  const [money, setMoney] = useState(0);
+  const [coinAmount, setCoinAmount] = useState(0);
+  const [selectedCoinID, setSelectecCoinID] = useState("");
+  
   //just one time
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -15,16 +20,48 @@ function App() {
 
   }, [])
   
-  return (
+  //Input Dollar Amount
+  const onChange = (event) => {
+    setMoney(event.target.value)
+  };
   
+  //Selected Coin
+  const selectedChanged = (event) => {
+    setSelectecCoinID(event.target.value)
+  };
+  
+  const onClick = (event) => {
+    const selectedCoin = coins.find(c => c.id === selectedCoinID);
+    if (selectedCoin) {
+      const coinPrice = selectedCoin.quotes.USD.price;
+      setCoinAmount(money / coinPrice)
+    }
+  };
+  
+  return (
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
-      <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}) = {coin.quotes.USD.price} USD</option>)}
+      <select onChange={selectedChanged} value={selectedCoinID}>
+       <option value="">Select a coin</option>
+        {coins.map((coin) => (
+          <option key={coin.id} value={coin.id}>
+            {coin.name} ({coin.symbol}) = {coin.quotes.USD.price} USD
+          </option>
+        ))}
       </select>
 
-      
+      <div>
+        <input
+          onChange={onChange}
+          type="number"
+          value={money}
+          placeHolder = "Please write down USD">
+        </input>
+        <button
+          onClick={onClick}
+        >Calculate</button>  ➡️ {coinAmount}
+      </div>
     </div>
   )
 }
